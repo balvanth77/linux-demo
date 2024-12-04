00c4f9ac5d4959f95880117d7d2f3a5245b4f8a5
balvanth <balvanthmangapuram@gmail.com>
2024-12-04 12:39:39 +0530

    first commit

--- applicationregisterservice/src/main/java/com/example/applicationregisterservice/dto/ApplicationRegisterDto.java
+++ applicationregisterservice/src/main/java/com/example/applicationregisterservice/dto/ApplicationRegisterDto.java
@@ -14,52 +14,7 @@ public class ApplicationRegisterDto {
     private String dateOfBirth;
     private Long ssn;
     private String caseNumber;
-	public String getFullName() {
-		return fullName;
-	}
-	public void setFullName(String fullName) {
-		this.fullName = fullName;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public Long getMobileNumber() {
-		return mobileNumber;
-	}
-	public void setMobileNumber(Long mobileNumber) {
-		this.mobileNumber = mobileNumber;
-	}
-	public String getGender() {
-		return gender;
-	}
-	public void setGender(String gender) {
-		this.gender = gender;
-	}
-	public String getDateOfBirth() {
-		return dateOfBirth;
-	}
-	public void setDateOfBirth(String dateOfBirth) {
-		this.dateOfBirth = dateOfBirth;
-	}
-	public Long getSsn() {
-		return ssn;
-	}
-	public void setSsn(Long ssn) {
-		this.ssn = ssn;
-	}
-	public String getCaseNumber() {
-		return caseNumber;
-	}
-	public void setCaseNumber(String caseNumber) {
-		this.caseNumber = caseNumber;
-	}
-	public ApplicationRegisterDto() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
+
     
     
     
